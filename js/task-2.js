7efc24ff35dd980e3e3a2b5dc6a86a7b49ed053f
KrasMish <mikekrasin2005@gmail.com>
2024-01-17 20:29:27 +0200

    new-tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,30 +1,33 @@
-// Задача 2. Форматування повідомлення
+// Задача 2. Композиція масивів
 // ВИКОНУЙ ЦЕ ЗАВДАННЯ У ФАЙЛІ task-2.js
-// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок(параметр message) та перевіряє його довжину
-//  відповідно до заданої максимальної довжини(параметр maxLength).
+// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray(масив), secondArray(масив) і
+// maxLength(число).Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі
+//  елементи з secondArray.
 
-// Доповни код функції таким чином, що:
-
-// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
-// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці
-// та повертає обрізану версію.
+// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength
+// елементів.
+// В іншому випадку функція повинна повернути весь новий масив.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.У консоль будуть виведені
-// результати її роботи.
+//  результати її роботи.
 
 
-function formatMessage(message, maxLength)
-{
-    if (message.length <= maxLength) return message;
-    else return `${message.slice(0, maxLength)}...`;
+function makeArray(arr1, arr2, maxLength) {
+    let arrTotal = arr1.slice(arr1);
+    for (let a of arr2)
+    {
+        arrTotal.push(a);
+
+    }
+    if (arrTotal.length > maxLength) return arrTotal.slice(0, maxLength);
+    else return arrTotal;
 }
 
 
-
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 
